707a2cdda05a4a29bdf01b465d66793190d1a86e
shubhampratap1804 <samsunggalaxygrand2pratap@gmail.com>
2021-08-21 09:00:21 +0530

    Created: method to create a new contact

--- src/main/java/com/main/AddressBookMain.java
+++ src/main/java/com/main/AddressBookMain.java
@@ -6,6 +6,6 @@ import java.util.Scanner;
 
 public class AddressBookMain {
     public static void main(String[] args){
-        System.out.println("Welcome to address book system");
+        System.out.println("Welcome to address book system simulation");
     }
 }
